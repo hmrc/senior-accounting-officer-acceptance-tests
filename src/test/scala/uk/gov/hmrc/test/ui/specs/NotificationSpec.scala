ecb64e89e233d11f910b905b7b3cf127ef5f69a6
AprilGar <april.garlington@hotmail.co.uk>
2026-01-19 16:01:29 +0000

    WIP SubmitNotificationSpec tests

---
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/NotificationSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.specs
+
+import uk.gov.hmrc.test.ui.adt.PageSectionStatus.{CannotStartYet, NotStarted}
+import uk.gov.hmrc.test.ui.adt.RegistrationPageLink.EnterYourCompanyDetailsLink
+import uk.gov.hmrc.test.ui.adt.RegistrationPageSection.{CompanyDetails, ContactDetails}
+import uk.gov.hmrc.test.ui.pages.AuthorityWizardPage
+import uk.gov.hmrc.test.ui.pages.submission.notification.*
+import uk.gov.hmrc.test.ui.specs.tags.{RegistrationTests, ZapTests}
+import uk.gov.hmrc.test.ui.support.AffinityGroup.Organisation
+import uk.gov.hmrc.test.ui.support.PageSupport.{assertOnPage, pageUrl}
+
+class NotificationSpec extends BaseSpec {
+
+  Feature("Submit Notification") {
+
+    Scenario(
+      "The submit notification start page displays with the correct page elements and values when starting a new submission",
+      RegistrationTests,
+      ZapTests
+    ) {
+      Given("an authenticated user lands on the registration page to register a business")
+      SubmitNotificationStartPage.navigateTo(pageUrl)
+      assertOnPage(SubmitNotificationStartPage)
+
+//      Then("the page displays with the expected page elements and values required at the start of a new registration")
+//      RegistrationPage.assertLinkIsVisibleWithText(EnterYourCompanyDetailsLink)
+//      RegistrationPage.assertSectionStatus(CompanyDetails, NotStarted)
+//      RegistrationPage.assertEnterYourContactDetailsLinkNotFound()
+//      RegistrationPage.assertSectionStatus(ContactDetails, CannotStartYet)
+//      RegistrationPage.assertSubmitButtonDoesNotExist()
+    }
+  }
+
+}
