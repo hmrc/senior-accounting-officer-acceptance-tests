343221767d86a6ffb23413dc2f25489bf3a6160b
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-01-29 11:36:33 +0000

    amend title

--- src/test/scala/uk/gov/hmrc/test/ui/pages/submission/certificate/CheckYourAnswersPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/submission/certificate/CheckYourAnswersPage.scala
@@ -25,8 +25,5 @@ object CheckYourAnswersPage extends BasePage {
     s"${TestConfiguration.url("senior-accounting-officer-submission-frontend")}/certificate/check-your-answers"
 
   override val pageTitle: String =
-    "Submit a notification - Senior Accounting Officer notification and certificate - GOV.UK"
-
-  val additionalInformationValueElement: By = By.cssSelector(".govuk-summary-list__value")
-  val additionalInformationChangeLink: By   = By.cssSelector("""[data-test-id="change-additional-information-link"]""")
+    "Submit a certificate - Senior Accounting Officer notification and certificate - GOV.UK"
 }
