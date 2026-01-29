f438d85f2f55f0746c7fe49350392800ba3a6f6b
Rishi Sachania <19263328+rishisachania@users.noreply.github.com>
2026-01-29 11:39:50 +0000

    Added certificate submission CheckYourAnswersPage (#29)
    * Added certificate submission CheckYourAnswersPage
    
    * amend title
    
    ---------
    
    Co-authored-by: jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/submission/certificate/CheckYourAnswersPage.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.pages.submission.certificate
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.test.ui.conf.TestConfiguration
+import uk.gov.hmrc.test.ui.pages.BasePage
+
+object CheckYourAnswersPage extends BasePage {
+  override val pageUrl: String =
+    s"${TestConfiguration.url("senior-accounting-officer-submission-frontend")}/certificate/check-your-answers"
+
+  override val pageTitle: String =
+    "Submit a certificate - Senior Accounting Officer notification and certificate - GOV.UK"
+}
