fe9693a4577dc58c11ea21e576850d77ef5d7e98
dananjaiahnemmadi <202163272+dananjaiahnemmadi@users.noreply.github.com>
2026-01-30 10:54:54 +0000

    added Submit Certificate Submitter page (#32)

--- src/test/scala/uk/gov/hmrc/test/ui/pages/submission/certificate/SubmitCertificateSubmitterPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/submission/certificate/SubmitCertificateSubmitterPage.scala
@@ -19,9 +19,9 @@ package uk.gov.hmrc.test.ui.pages.submission.certificate
 import uk.gov.hmrc.test.ui.conf.TestConfiguration
 import uk.gov.hmrc.test.ui.pages.BasePage
 
-object CheckYourAnswersPage extends BasePage {
+object SubmitCertificateSubmitterPage extends BasePage {
   override val pageUrl: String =
-    s"${TestConfiguration.url("senior-accounting-officer-submission-frontend")}/certificate/check-your-answers"
+    s"${TestConfiguration.url("senior-accounting-officer-submission-frontend")}/certificate/submit-certificate-submitter"
 
   override val pageTitle: String =
     "Submit a certificate - Senior Accounting Officer notification and certificate - GOV.UK"
