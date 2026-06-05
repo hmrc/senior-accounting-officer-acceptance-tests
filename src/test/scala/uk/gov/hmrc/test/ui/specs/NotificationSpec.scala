fce823240c980c8b763f5e17faccbecc6ec2edf8
geetapunna-qa <267024261+geetapunna-qa@users.noreply.github.com>
2026-06-05 16:39:41 +0100

    Update src/test/scala/uk/gov/hmrc/test/ui/specs/NotificationSpec.scala
    Co-authored-by: Michael Arveda <222641308+Michael-A-NC@users.noreply.github.com>

--- src/test/scala/uk/gov/hmrc/test/ui/specs/NotificationSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/NotificationSpec.scala
@@ -20,7 +20,7 @@ import uk.gov.hmrc.test.ui.adt.AffinityGroup.Organisation
 import uk.gov.hmrc.test.ui.adt.NotificationTaskListSection.*
 import uk.gov.hmrc.test.ui.adt.PageSectionStatus.{CannotStartYet, Completed, NotStarted}
 import uk.gov.hmrc.test.ui.pages.submission.*
-import uk.gov.hmrc.test.ui.pages.submission.notification.{CheckYourAnswersPage, *}
+import uk.gov.hmrc.test.ui.pages.submission.notification.*
 import uk.gov.hmrc.test.ui.pages.{AccountHomePage, AuthorityWizardPage}
 import uk.gov.hmrc.test.ui.specs.tags.{SubmissionUITests, ZapTests}
 import uk.gov.hmrc.test.ui.support.PageSupport.*
