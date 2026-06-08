bd4487903b555906689625cd88a7e3e2b6ff2be4
Michael-A-NC <222641308+Michael-A-NC@users.noreply.github.com>
2026-06-08 09:25:18 +0100

    Adds test coverage for review your comapnies page
    Adds csv file and renames another

--- src/test/scala/uk/gov/hmrc/test/ui/pages/submission/notification/UploadSubmissionTemplatePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/submission/notification/UploadSubmissionTemplatePage.scala
@@ -21,7 +21,7 @@ import org.openqa.selenium.{By, WebDriver}
 import org.scalatest.matchers.should.Matchers.*
 import uk.gov.hmrc.test.ui.conf.TestConfiguration
 import uk.gov.hmrc.test.ui.pages.CommonPage
-import uk.gov.hmrc.test.ui.support.PageSupport.{assertTextOnPage, clickElement}
+import uk.gov.hmrc.test.ui.support.PageSupport.{clickElement, extractRelativeUrl}
 import uk.gov.hmrc.test.ui.support.SubmissionButtonSupport
 
 import scala.concurrent.duration.*
@@ -36,40 +36,24 @@ object UploadSubmissionTemplatePage extends CommonPage with SubmissionButtonSupp
   override val pageTitle: String =
     "Upload a submission template for your notification - Senior Accounting Officer notification and certificate - GOV.UK"
 
-  private val hiddenFileInputLocator = By.cssSelector(".govuk-file-upload")
+  private val hiddenFileInputLocator: By = By.cssSelector(".govuk-file-upload")
+  val pageHeadingElement: By             = By.cssSelector("h1")
 
   def verifyTemplateGuidanceLink(): Unit = {
-    val guidanceLink =
-      driver.findElement(By.id("template-guidance"))
-
-    guidanceLink.getAttribute("target") shouldBe "_blank"
-
-    guidanceLink
-      .getAttribute("href")
-      .contains("/senior-accounting-officer/submission/template-guidance") shouldBe true
+    val expectedTemplateGuidanceLinkHrefValue = "/senior-accounting-officer/submission/template-guidance"
+    val guidanceLink                          = driver.findElement(By.id("template-guidance"))
+    guidanceLink.getAttribute("target") mustBe "_blank"
+    extractRelativeUrl(guidanceLink.getAttribute("href")) mustBe expectedTemplateGuidanceLinkHrefValue
   }
-
-  private def fluentWaitWithLongDelay: FluentWait[WebDriver] =
-    fluentWait(timeout = 7.seconds, polling = 250.milliseconds)
 
   override def clickSubmissionButton(): Unit = {
     clickElement(submissionButtonLocator)
-    fluentWaitWithLongDelay.until(
-      ExpectedConditions.textToBePresentInElementLocated(
-        By.cssSelector("h1"),
-        "Review the companies in your notification"
-      )
-    )
+    waitForTextInHeading("Review the companies in your notification")
   }
 
   def clickSubmissionButtonExpectingTemplateError(): Unit = {
     clickElement(submissionButtonLocator)
-    fluentWaitWithLongDelay.until(
-      ExpectedConditions.textToBePresentInElementLocated(
-        By.cssSelector("h1"),
-        "There is a problem with your submission template file"
-      )
-    )
+    waitForTextInHeading("There is a problem with your submission template file")
   }
 
   def chooseFile(resourceName: String): Unit = {
@@ -79,8 +63,12 @@ object UploadSubmissionTemplatePage extends CommonPage with SubmissionButtonSupp
     driver.findElement(hiddenFileInputLocator).sendKeys(absolutePath)
   }
 
-  val pageHeadingElement: By                   = By.cssSelector(".govuk-fieldset__heading")
-  def assertHeadingMatches(text: String): Unit = {
-    assertTextOnPage(pageHeadingElement, text)
+  private def fluentWaitWithLongDelay: FluentWait[WebDriver] =
+    fluentWait(timeout = 7.seconds, polling = 250.milliseconds)
+
+  private def waitForTextInHeading(text: String): Unit = {
+    fluentWaitWithLongDelay.until(
+      ExpectedConditions.textToBePresentInElementLocated(pageHeadingElement, text)
+    )
   }
 }
--- src/test/scala/uk/gov/hmrc/test/ui/pages/submission/notification/UploadTablePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/submission/notification/UploadTablePage.scala
@@ -16,8 +16,11 @@
 
 package uk.gov.hmrc.test.ui.pages.submission.notification
 
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.{By, WebDriver}
 import uk.gov.hmrc.test.ui.conf.TestConfiguration
 import uk.gov.hmrc.test.ui.pages.CommonPage
+import uk.gov.hmrc.test.ui.support.PageSupport.clickElement
 import uk.gov.hmrc.test.ui.support.SubmissionButtonSupport
 
 object UploadTablePage extends CommonPage with SubmissionButtonSupport {
@@ -27,5 +30,44 @@ object UploadTablePage extends CommonPage with SubmissionButtonSupport {
 
   override val pageTitle: String = {
     "Review the companies in your notification - Senior Accounting Officer notification and certificate - GOV.UK"
+  }
+
+  val pageContentElement: By        = By.cssSelector(".govuk-grid-column-two-thirds")
+  val paragraph: By                 = By.cssSelector(".govuk-body")
+  val uploadUpdatedTemplateLink: By = By.cssSelector(".govuk-body a")
+  val tableLocator: By              = By.cssSelector("table")
+  val tableEmptyMessageLocator: By  = By.cssSelector("tbody tr td")
+  val tableRowLocator: By           = By.cssSelector("tbody tr")
+
+  private def textInParagraphTwo: String = fluentWait
+    .until(ExpectedConditions.visibilityOfElementLocated(pageContentElement))
+    .findElements(paragraph)
+    .get(1)
+    .getText
+
+  def assertCompanyCountAndSaoMatch(companyCount: Int, expectedSao: String): Unit = {
+    val expectedText = s"There were $companyCount companies $expectedSao was responsible for during the financial year."
+    textInParagraphTwo mustBe expectedText
+  }
+
+  def assertCompanyTableEmpty(): Unit = {
+    val messageInEmptyTable: String = fluentWait
+      .until(ExpectedConditions.visibilityOfElementLocated(tableEmptyMessageLocator))
+      .getText
+
+    messageInEmptyTable mustBe "No parsed rows"
+  }
+
+  def assertCompanyCountInTableEquals(expectedCount: Int): Unit = {
+    val rowCount: Int = fluentWait
+      .until(ExpectedConditions.visibilityOfElementLocated(tableLocator))
+      .findElements(tableRowLocator)
+      .size()
+
+    rowCount mustBe expectedCount
+  }
+
+  def clickUploadUpdatedTemplateLink(): Unit = {
+    clickElement(uploadUpdatedTemplateLink)
   }
 }
