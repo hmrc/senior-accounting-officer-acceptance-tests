826511176263c904ab4a0a3a86b07bccd9b07442
geetapunna-qa <267024261+geetapunna-qa@users.noreply.github.com>
2026-06-19 13:55:55 +0100

    removed commented out code

--- src/test/scala/uk/gov/hmrc/test/ui/pages/submission/notification/CheckYourAnswersPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/submission/notification/CheckYourAnswersPage.scala
@@ -27,7 +27,6 @@ object CheckYourAnswersPage extends CommonPage with SubmissionButtonSupport {
     s"${TestConfiguration.url("senior-accounting-officer-submission-frontend")}/notification/check-your-answers"
 
   override val pageTitle: String =
-//    "Submit a notification - Senior Accounting Officer notification and certificate - GOV.UK"
     "Check your answers - Senior Accounting Officer notification and certificate - GOV.UK"
 
   val additionalInformationValueElement: By = testId("additional-information-value")
